a4b6f9cf3bef2b9ab0a3cf5cd5c1f4bfb7da3557
Allen Lucke <allenlucke@gmail.com>
2021-05-12 17:27:10 -0500

    Added app config to angular fe

--- app/clientApp/angularFinancesFE/src/app/app.module.ts
+++ app/clientApp/angularFinancesFE/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { ExpensesComponent } from './expenses/expenses.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     AppRoutingModule
   ],
