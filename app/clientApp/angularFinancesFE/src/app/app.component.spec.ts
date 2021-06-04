c409ff3737fa852336d7d8156cc19e13de9116fa
Allen Lucke <allenlucke@gmail.com>
2021-06-04 17:20:31 -0500

    Updated angular fe

--- app/clientApp/angularFinancesFE/src/app/app.component.spec.ts
+++ app/clientApp/angularFinancesFE/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularFinancesFE'`, () => {
+  it(`should have as title 'angularFinancesFE'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularFinancesFE');
+    expect(app.title).toEqual('angularFinancesFE');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularFinancesFE app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angularFinancesFE app is running!');
   });
 });
