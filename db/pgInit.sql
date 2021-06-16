7915180f59db56c85eb00d87f59d2a3e6bedfb22
Allen Lucke <allenlucke@gmail.com>
2021-06-16 17:21:56 -0500

    Added a postgres init file

---
+++ db/pgInit.sql
@@ -0,0 +1,66 @@
+CREATE TABLE "users" (
+	"id" SERIAL PRIMARY KEY,
+	"firstName" VARCHAR(20) NOT NULL,
+	"lastName" VARCHAR(30) NOT NULL,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "securityLevel" int default 1 NOT NULL,
+    "email" VARCHAR (80),
+    "role" VARCHAR (80),
+	"created" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "last_active" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "isActive" BOOLEAN default TRUE
+);
+
+CREATE TABLE "period" (
+	"id" SERIAL PRIMARY KEY,
+	"start_date" TIMESTAMP WITH TIME ZONE,
+	"end_date" TIMESTAMP WITH TIME ZONE,
+	"users_id" INT NOT NULL REFERENCES "users"
+);
+
+CREATE TABLE "budget" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR (80) UNIQUE NOT NULL
+);
+
+CREATE TABLE "budget_period" (
+  "id" SERIAL PRIMARY KEY,
+  "budget_id" INT NOT NULL REFERENCES "budget",
+  "period_id" INT NOT NULL REFERENCES "period",
+  "isClosed" BOOLEAN default FALSE
+);
+
+CREATE TABLE "expenseCategory" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR (80) UNIQUE NOT NULL
+);
+
+CREATE TABLE "budget_expenseCategory" (
+  "id" SERIAL PRIMARY KEY,
+  "budget_id" INT NOT NULL REFERENCES "budget",
+  "expenseCategory_id" INT NOT NULL REFERENCES "expenseCategory",
+  "amountBudgeted" NUMERIC(12,2) DEFAULT 0.00,
+  "amountPaid" NUMERIC(12,2) DEFAULT 0.00,
+  "dueDate" TIMESTAMP WITH TIME ZONE,
+  "datePaidInFull" TIMESTAMP WITH TIME ZONE
+);
+
+CREATE TABLE "account" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR (80) UNIQUE NOT NULL,
+  "balance" NUMERIC(12,2),
+  "availBalance" NUMERIC(12,2),
+  "isCredit" BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE "expenseItems" (
+  "id" SERIAL PRIMARY KEY,
+  "budget_expenseCategory_id" INT REFERENCES "budget_expenseCategory",
+  "name" VARCHAR (80) NOT NULL,
+  "datePaid" TIMESTAMP WITH TIME ZONE,
+  "isPaid" BOOLEAN,
+  "account_id" INT NOT NULL REFERENCES "account"
+);
+
+
