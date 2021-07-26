99c29ce0b8165f2c17914ab3acd3419d6565faa8
Allen Lucke <allenlucke@gmail.com>
2021-07-26 17:45:45 -0500

    Added acctBal.sql

---
+++ db/acctBalScratch.sql
@@ -0,0 +1,41 @@
+
+
+--GET expenseItems by account/period
+SELECT "expenseItem".id, "expenseItem"."transactionDate", "account".name AS "accountName" , "period".id AS "periodId", 
+"expenseItem".name AS "expenseItemName", "account".id AS "accountId", "expenseItem".amount FROM "expenseItem"
+JOIN "account" ON "expenseItem"."account_id" = "account".id
+JOIN "budget_expenseCategory" ON "expenseItem"."budget_expenseCategory_id" = "budget_expenseCategory".id
+JOIN "budget" ON "budget_expenseCategory".id = "budget".id
+JOIN "period" ON "budget".period_id = "period".id
+JOIN "accountPeriod" ON "period".id = "accountPeriod".period_id
+WHERE "expenseItem"."transactionDate" >= "period"."startDate" AND "expenseItem"."transactionDate" <= "period"."endDate"
+AND "account".id =1
+AND "period".id = 1
+ORDER BY "expenseItem"."transactionDate";
+
+
+--Get count of open periods prior to period's startdate
+SELECT COUNT(*) FROM "period" 
+JOIN "budget" ON "period".id = "budget".period_id
+WHERE "startDate" < '08/01/2021'
+AND "budget"."isClosed" = 'false';
+
+
+--Get startdate of last unclosed period
+SELECT * FROM "period" 
+JOIN "budget" ON "period".id = "budget".period_id
+WHERE "period"."startDate" < '08/01/2021'
+AND "budget"."isClosed" = 'false'
+ORDER BY "period"."startDate" ASC
+LIMIT 1;
+
+UPDATE "budget" SET "isClosed" = false WHERE "budget".id =1;
+
+--Get beggining balance of last unclosed period
+SELECT "accountPeriod"."beginningBalance" FROM "period" 
+JOIN "budget" ON "period".id = "budget".period_id
+JOIN "accountPeriod" ON "period".id = "accountPeriod".period_id
+WHERE "period"."startDate" < '08/01/2021'
+AND "budget"."isClosed" = 'false'
+ORDER BY "period"."startDate" ASC
+LIMIT 1;
